ada8fdb34c08077fc6fa09ed30632e72980757d1
JulianaAparicio <juliana_aparicio@outlook.com>
2022-11-09 22:26:16 -0300

    Added test to toggleFavorite method

--- BackEnd/booking/booking/src/test/java/digital/booking/services/FavoriteServiceTest.java
+++ BackEnd/booking/booking/src/test/java/digital/booking/services/FavoriteServiceTest.java
@@ -3,6 +3,7 @@ package digital.booking.services;
 import digital.booking.DTO.Favorite;
 import digital.booking.DTO.ProductDTO;
 import digital.booking.entities.*;
+import digital.booking.exceptions.BadRequestException;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,7 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest
@@ -32,17 +33,29 @@ public class FavoriteServiceTest {
         List<Rating> ratingsTest = new ArrayList<>();
         List<Product> productsTest = new ArrayList<>();
 
-        productDTOTest = new ProductDTO(1L, "titleTest", "descriptionTest", categoryTest, amenitiesTest, locationTest, imagesTest, itemsTest, ratingsTest);
-        userTest = new User(1L, "userNameTest", "userLastNameTest", "userEmailTest", "userPassTest", productsTest);
+        productDTOTest = new ProductDTO(25L, "titleTest", "descriptionTest", categoryTest, amenitiesTest, locationTest, imagesTest, itemsTest, ratingsTest);
+        userTest = new User(25L, "userNameTest", "userLastNameTest", "userEmailTest", "userPassTest", productsTest);
         favoriteTest = new Favorite(userTest.getId(), productDTOTest.getId());
     }
 
     @Test
     void testToggleFavorite() {
-        /*try {
+        try {
+
+        //GIVEN
+        Long userId = userTest.getId();
+        Long productId = productDTOTest.getId();
+
+        // WHEN
+        favoriteService.toggleFavorite(userId,productId);
+
+        // THEN
+        List<Product> userTestProducts = userTest.getProducts();
+        assertTrue(userTestProducts.size() > 0,"There are no products associated to this user.");
+
         } catch (BadRequestException e) {
             e.printStackTrace();
-        }*/
+        }
     }
 
 }
