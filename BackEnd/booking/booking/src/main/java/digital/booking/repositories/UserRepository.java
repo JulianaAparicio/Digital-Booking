b755b0047e6b54446d6994c196a5437224c2a9a0
JulianaAparicio <juliana_aparicio@outlook.com>
2022-11-17 19:12:53 -0300

    Deleted unusued import

--- BackEnd/booking/booking/src/main/java/digital/booking/repositories/UserRepository.java
+++ BackEnd/booking/booking/src/main/java/digital/booking/repositories/UserRepository.java
@@ -1,6 +1,5 @@
 package digital.booking.repositories;
 
-import digital.booking.entities.Product;
 import digital.booking.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
