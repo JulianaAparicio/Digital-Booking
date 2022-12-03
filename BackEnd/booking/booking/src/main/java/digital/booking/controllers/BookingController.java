5dfae68c88d26074cecf84cca9be2a039f97a78f
JulianaAparicio <juliana_aparicio@outlook.com>
2022-12-02 21:03:48 -0300

    Deleted unused imports.

--- BackEnd/booking/booking/src/main/java/digital/booking/controllers/BookingController.java
+++ BackEnd/booking/booking/src/main/java/digital/booking/controllers/BookingController.java
@@ -10,11 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.Locale;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/booking")
