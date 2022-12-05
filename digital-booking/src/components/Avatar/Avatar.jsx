d20e8935197b4b57923b56426853d6791d1d5839
agustinaf-87 <fernandez.agustinaelena@gmail.com>
2022-12-05 20:41:48 -0300

    changes on dropdown menu user

--- digital-booking/src/components/Avatar/Avatar.jsx
+++ digital-booking/src/components/Avatar/Avatar.jsx
@@ -1,16 +1,27 @@
-import React, { useState } from 'react'
+import React, { createRef, useState } from 'react'
 import CloseIcon from '../../shared/Icons/CloseIcon';
 import './Avatar.scss';
 import { Link } from "react-router-dom";
 import HeartIcon from "../../shared/Icons/HeartIcon"
 import ReservationIcon from "../../shared/Icons/ReservationIcon"
+import gsap from "gsap";
 
 
 export const Avatar = ({ name, lastName, logOut }) => {
 
+  const optionsMenu = createRef()
+
+  function optionsAppear() {
+    gsap.fromTo(optionsMenu.current,{display:"block", opacity:0, yPercent: -50}, {opacity:1, yPercent: 0, duration: 0.1})
+  }
+  
+  function optionsDessapear(){
+    gsap.to(optionsMenu.current, {opacity: 0, duration: 0.1, display: "none"})
+  }
+
   return (
-    <div className='db-avatar'>
-      <span className='db-avatar-initials'>
+    <div className='db-avatar' onMouseLeave={optionsDessapear}>
+      <span className='db-avatar-initials' onMouseOver={optionsAppear}>
         {name && name.charAt(0)} {lastName && lastName.charAt(0)}
       </span>
       <div className='db-user-nav'>
@@ -18,19 +29,28 @@ export const Avatar = ({ name, lastName, logOut }) => {
           <span>Hola,</span>
           <span>{name} {lastName}</span>     
         </div>
-          <ul>
-            <Link to={"/myreservations"}>
-              <li className='dropdown-item'>Mis Reservas</li>
-              <div className='dropdown-icon'><ReservationIcon /></div>
-            </Link>
-            <Link to={"/myreservations"}>
-              <li className='dropdown-item'>Mis favoritos</li>
-              <div className='dropdown-icon'><HeartIcon /></div>
-            </Link>
-            <li className='dropdow-item' onClick={logOut}>Cerrar Sesión</li>
-            <div className='dropdown-icon'><CloseIcon /></div>
-          </ul>
+          
       </div>
+      <ul className='db-user-options' ref={optionsMenu}>
+            <Link to={"/myreservations"}>
+              <li className='dropdown-item'>
+              <span>Mis Reservas</span>
+              <div className='dropdown-icon'><ReservationIcon /></div>
+              </li>
+            </Link>
+            <Link to={"/myreservations"}>
+              <li className='dropdown-item'>
+              <span>Mis favoritos</span>
+              <div className='dropdown-icon'><HeartIcon /></div></li>
+              
+            </Link>
+            <li className='dropdow-item' onClick={logOut}>
+              <span>Cerrar Sesión</span>
+              <div className='dropdown-icon'><CloseIcon /></div>
+            </li>
+            
+            
+          </ul>
     </div>
   )
 }
