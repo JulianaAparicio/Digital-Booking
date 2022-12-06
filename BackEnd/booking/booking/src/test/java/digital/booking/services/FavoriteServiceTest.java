41d40b91d51531c6b0b03eee791174468fe11b71
JulianaAparicio <juliana_aparicio@outlook.com>
2022-12-05 21:04:29 -0300

    Added mockito library to test toggle method

--- BackEnd/booking/booking/src/test/java/digital/booking/services/FavoriteServiceTest.java
+++ BackEnd/booking/booking/src/test/java/digital/booking/services/FavoriteServiceTest.java
@@ -1,60 +1,120 @@
 package digital.booking.services;
 
 import digital.booking.DTO.FavoriteDTO;
-import digital.booking.DTO.ImageDTO;
-import digital.booking.DTO.ProductDTO;
-import digital.booking.DTO.RatingDTO;
 import digital.booking.entities.*;
 import digital.booking.exceptions.BadRequestException;
+import digital.booking.repositories.*;
 import org.junit.jupiter.api.*;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @SpringBootTest
 public class FavoriteServiceTest {
-    @Autowired
+    @InjectMocks
     private FavoriteService favoriteService;
 
+    @Mock
+    ProductRepository productRepository;
+
+    @Mock
+    CategoryRepository categoryRepository;
+
+    @Mock
+    CityRepository cityRepository;
+
+    @Mock
+    LocationRepository locationRepository;
+
+    @Mock
+    RoleRepository roleRepository;
+
+    @Mock
+    UserRepository userRepository;
+
+    @Mock
+    private Category categoryTest;
+
+    @Mock
+    private City cityTest;
+
+    @Mock
+    private Location locationTest;
+
+    @Mock
+    private Role roleTest;
+
     private User userTest;
-    private ProductDTO productDTOTest;
+    private Product productTest;
+
     private FavoriteDTO favoriteTest;
 
     @BeforeEach
     void setUp() {
-        Category categoryTest = new Category();
-        List<Amenity> amenitiesTest = new ArrayList<>();
-        Location locationTest = new Location();
-        List<ImageDTO> imagesTest = new ArrayList<>();
-        List<Item> itemsTest = new ArrayList<>();
-        List<RatingDTO> ratingsTest = new ArrayList<>();
-        List<Product> productsTest = new ArrayList<>();
+        MockitoAnnotations.openMocks(this);
 
-        productDTOTest = new ProductDTO(25L, "titleTest", "descriptionTest", categoryTest, amenitiesTest, locationTest, imagesTest, itemsTest, ratingsTest, new ArrayList<>());
-        userTest = new User("userNameTest", "userLastNameTest", "userEmailTest", "userPassTest", productsTest);
-        userTest.setId(10L);
-        favoriteTest = new FavoriteDTO(userTest.getId(), productDTOTest.getId());
+        categoryTest = new Category(1L,"titleTest","descriptionTest","imageURLTest");
+        categoryRepository.save(categoryTest);
+
+        cityTest = new City(1L,"cityNameTest","stateTest","countryTest");
+        cityRepository.save(cityTest);
+
+        locationTest = new Location(1L,cityTest,"addressTest","latitudeTest","longitudeTest");
+        locationRepository.save(locationTest);
+
+        productTest = new Product(1L, "titleTest", "descriptionTest",categoryTest,locationTest);
+        productRepository.save(productTest);
+
+        List<Product> productListTest = new ArrayList<>();
+        productListTest.add(productTest);
+
+        userTest = new User(1L,"nameTest","lastNameTest", "emailTest",
+                "passwordTest",true,roleTest);
+        userTest.setProducts(productListTest);
+        userRepository.save(userTest);
+
+        favoriteTest = new FavoriteDTO(userTest.getId(), productTest.getId());
     }
 
     @Test
     void testToggleFavorite() {
         try {
+            when(productRepository.findById(productTest.getId())).thenReturn(Optional.ofNullable(productTest));
+            when(userRepository.findById(userTest.getId())).thenReturn(Optional.ofNullable(userTest));
 
-        //GIVEN
-        Long userId = userTest.getId();
-        Long productId = productDTOTest.getId();
+            Long userId = userTest.getId();
+            Long productId = productTest.getId();
 
-        // WHEN
-        favoriteService.toggleFavorite(userId,productId);
+            // Verifies not null attributes:
+            assertNotNull(userId,"The value is null.");
+            assertNotNull(productId,"The value is null.");
+            assertNotNull(favoriteTest,"The value is null.");
 
-        // THEN
-        List<Product> userTestProducts = userTest.getProducts();
-        assertTrue(userTestProducts.size() > 0,"There are no products associated to this user.");
+            List<Product> userTestProducts = userTest.getProducts();
+
+            // Remove from favorite list:
+            favoriteService.toggleFavorite(userId,productId);
+
+            assertNotNull(userTestProducts,"The value is null.");
+
+            assertEquals(0, userTestProducts.size(), "There are still products associated to this user.");
+
+            // Add to favorite list:
+            favoriteService.toggleFavorite(userId,productId);
+
+            assertTrue(userTestProducts.size() > 0,"There are no products associated to this user.");
 
         } catch (BadRequestException e) {
             e.printStackTrace();
